02de19cac0c509a63e63fb59cd88457bbdaffdcb
Kumapooh723 <38307955+Kumapooh723@users.noreply.github.com>
2018-04-16 13:49:22 +0900

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,11 @@
+
+public class Sample {
+	public static void main(String[] args){
+		System.out.println("疲れました");
+		System.out.println("とても眠いです");
+		System.out.println("乗り気になりませんが、頑張ります");
+		for(int a=0;a<3; a++)System.out.println("気合いだー");
+		for(int b=1;b<4;b++)System.out.println("おい!");
+	}
+
+}
